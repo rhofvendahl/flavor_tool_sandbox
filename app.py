152b84828a92d3bd095ac59aff2855d445ce7ebf
russell Hofvendahl <rhofvendahl@gmail.com>
2020-10-22 19:10:45 -0700

    add google analytics tag

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 app = Flask(__name__)
+# app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.before_request
 def force_https():
